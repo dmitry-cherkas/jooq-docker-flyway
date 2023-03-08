d3bcb3d9ef392b3be1b892f6a797ff2a2147a7ff
Dmitry Cherkas <dmitry.cherkas@traderepublic.com>
2023-03-08 17:46:17 +0100

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.0.4"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+
+    id("org.flywaydb.flyway") version "9.14.1" // TODO version "6.4.3" works fine
+    id("com.revolut.jooq-docker") version "0.3.9"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation("org.jooq:jooq:3.14.15")
+    implementation("org.postgresql:postgresql:42.5.3")
+    jdbc("org.postgresql:postgresql:42.2.5")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/resources/db/migration/V000__add_test.sql
@@ -0,0 +1,5 @@
+CREATE TABLE test
+(
+    id   UUID NOT NULL primary key,
+    text TEXT NOT NULL
+)
